5e210a3a842b9780e42330df917b0229d2fcbe77
Cristian Florez <133178936+Criswa7@users.noreply.github.com>
2024-01-02 16:47:49 -0500

    fusion menu mobile w mediaquerys

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const actionBarMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+mobileMenu.classList.remove('active')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+actionBarMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('active')
 }
